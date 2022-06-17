65babc286311368bc3dd74f0a07f134bbf02e2c7
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-18 04:37:08 +0530

    add comments

--- Graded Coding Assignment 1/src/com/ashutosh/service/AdminDepartment.java
+++ Graded Coding Assignment 1/src/com/ashutosh/service/AdminDepartment.java
@@ -5,21 +5,21 @@ import com.ashutosh.model.SuperDepartment;
 
 public class AdminDepartment extends SuperDepartment { //inherits class SuperDepartment
 	
-	//class method to get department name
+	//subclass method to get department name
 	public String departmentName() {
 		
 		return "Admin Department";
 		
 	}
 	
-	//class method to get daily work assignment
+	//subclass method to get daily work assignment
 	public String getTodaysWork() {
 		
 		return "Complete your documents submission";
 		
 	}
 	
-	//class method to get assignment deadline
+	//subclass method to get assignment deadline
 	public String getWorkDeadline() {
 		
 		return "Complete by EOD";
--- Graded Coding Assignment 1/src/com/ashutosh/service/HRDepartment.java
+++ Graded Coding Assignment 1/src/com/ashutosh/service/HRDepartment.java
@@ -1,32 +1,32 @@
 package com.ashutosh.service;
 
-////import superclass
+//import superclass
 import com.ashutosh.model.SuperDepartment;
 
 public class HRDepartment extends SuperDepartment { //inherits class SuperDepartment
 	
-	//class method to get department name
+	//subclass method to get department name
 	public String departmentName() {
 		
 		return "HR Department";
 		
 	}
 	
-	//class method to get daily work assignment
+	//subclass method to get daily work assignment
 	public String getTodaysWork() {
 		
 		return "Fill today's worksheet and mark your attendance";
 		
 	}
 	
-	//class method to get assignment deadline
+	//subclass method to get assignment deadline
 	public String getWorkDeadline() {
 		
 		return "Complete by EOD";
 		
 	}
 	
-	//class method to get activity
+	//subclass method to get activity
 	public String doActivity() {
 		
 		return "team Lunch";

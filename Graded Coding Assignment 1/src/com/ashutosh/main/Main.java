c6c452f1d41428631f2c697869135a04500df1f9
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-17 08:17:00 +0530

    create object of AdminDepartment

--- Graded Coding Assignment 1/src/com/ashutosh/main/Main.java
+++ Graded Coding Assignment 1/src/com/ashutosh/main/Main.java
@@ -1,9 +1,12 @@
 package com.ashutosh.main;
 
+import com.ashutosh.model.SuperDepartment;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		SuperDepartment adminObj = new SuperDepartment();
 		
 
 	}

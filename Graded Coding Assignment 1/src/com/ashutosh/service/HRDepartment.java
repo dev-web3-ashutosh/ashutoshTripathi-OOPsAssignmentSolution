df7d7a765211c83c256aaee2db97b8174e04e8ea
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-18 04:34:15 +0530

    add comments

--- Graded Coding Assignment 1/src/com/ashutosh/service/HRDepartment.java
+++ Graded Coding Assignment 1/src/com/ashutosh/service/HRDepartment.java
@@ -1,27 +1,32 @@
 package com.ashutosh.service;
 
+////import superclass
 import com.ashutosh.model.SuperDepartment;
 
-public class HRDepartment extends SuperDepartment {
+public class HRDepartment extends SuperDepartment { //inherits class SuperDepartment
 	
+	//class method to get department name
 	public String departmentName() {
 		
 		return "HR Department";
 		
 	}
 	
+	//class method to get daily work assignment
 	public String getTodaysWork() {
 		
 		return "Fill today's worksheet and mark your attendance";
 		
 	}
 	
+	//class method to get assignment deadline
 	public String getWorkDeadline() {
 		
 		return "Complete by EOD";
 		
 	}
 	
+	//class method to get activity
 	public String doActivity() {
 		
 		return "team Lunch";

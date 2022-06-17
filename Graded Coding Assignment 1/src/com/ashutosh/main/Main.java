3697a48cc1fb3a51d4ddabcfa6cf1d97a7cc23b1
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-17 08:30:18 +0530

    write main method

--- Graded Coding Assignment 1/src/com/ashutosh/main/Main.java
+++ Graded Coding Assignment 1/src/com/ashutosh/main/Main.java
@@ -1,16 +1,37 @@
 package com.ashutosh.main;
 
-import com.ashutosh.model.SuperDepartment;
+import com.ashutosh.service.AdminDepartment;
+import com.ashutosh.service.HRDepartment;
+import com.ashutosh.service.TechDepartment;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		SuperDepartment adminObj = new SuperDepartment();
-		SuperDepartment hrObj = new SuperDepartment();
-		SuperDepartment techObj = new SuperDepartment();
+		AdminDepartment adminObj = new AdminDepartment();
+		HRDepartment hrObj = new HRDepartment();
+		TechDepartment techObj = new TechDepartment();
 		
+		System.out.println("Welcome to " + adminObj.departmentName());
+		System.out.println(adminObj.getTodaysWork());
+		System.out.println(adminObj.getWorkDeadline());
+		System.out.println(adminObj.isTodayAHoliday());
 		
+		System.out.println();
+		
+		System.out.println("Welcome to " + hrObj.departmentName());
+		System.out.println(hrObj.doActivity());
+		System.out.println(hrObj.getTodaysWork());
+		System.out.println(hrObj.getWorkDeadline());
+		System.out.println(hrObj.isTodayAHoliday());
+		
+		System.out.println();
+		
+		System.out.println("Welcome to " + techObj.departmentName());
+		System.out.println(techObj.getTodaysWork());
+		System.out.println(techObj.getWorkDeadline());
+		System.out.println(techObj.getTechStackInformation());
+		System.out.println(techObj.isTodayAHoliday());
 
 	}
 
